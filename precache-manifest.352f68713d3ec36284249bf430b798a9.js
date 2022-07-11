59646cfcb2113c27ade49b007ba7fde99fd1de93
Vishal Tanna <vishal.tanna2010@gmail.com>
2022-07-11 11:30:20 +0530

    Updates

---
+++ precache-manifest.352f68713d3ec36284249bf430b798a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "06bb7d478cc0a33b4516",
+    "url": "/Vish05/starting-react/static/css/main.55d38f3e.chunk.css"
+  },
+  {
+    "revision": "06bb7d478cc0a33b4516",
+    "url": "/Vish05/starting-react/static/js/main.06bb7d47.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/Vish05/starting-react/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "3a71f885181f27aa860c",
+    "url": "/Vish05/starting-react/static/js/2.3a71f885.chunk.js"
+  },
+  {
+    "revision": "c8c59b42b5361903fe17",
+    "url": "/Vish05/starting-react/static/js/runtime~main.c8c59b42.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Vish05/starting-react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "8007cbe9c1215dc46242c04c048a2fb0",
+    "url": "/Vish05/starting-react/index.html"
+  }
+];
